7eff3730900e1cfdb2e9c4df4943f90b4ff2ff46
abeGong <abeGong@gmail.com>
2010-08-18 16:52:05 +0000

    Updated faux_accio.py script.  Not functional at the moment.

--- README.txt
+++ README.txt
@@ -4,10 +4,49 @@ Labeling data for text classification can be a huge pain. In 2004, Davidov et al
 
 This project recreates an Accio-like framework in python. The goal is to automate the acquisition of "labeled" data for text classifiers.
 
+===== Mainfest =====
+
+* faux_accio.py
+* convert.py
+* content_tree.xml.bz2
+
 ===== Setup =====
 
-1. Go to the Open Directory Project and download the content dump file (http://rdf.dmoz.org/rdf/content.rdf.u8.gz).  This file contains the contents of the ODP.  Unzip it to the project directory.
+1a. Go to the Open Directory Project and download the content dump file (http://rdf.dmoz.org/rdf/content.rdf.u8.gz).  This file contains the contents of the ODP.  Unzip it to the project directory.
 
-2. Run convert.py.  You only need to do this once to create the reduced xml file content_tree.xml.
+2a. Run convert.py.  You only need to do this once to create the reduced xml file content_tree.xml.
+
+  --- OR ---
+
+1b. Just unpack content_tree.xml.bz2.  It may be out of date, but will spare you the download and conversion.
 
 ===== Usage =====
+faux-accio.py [n] [node1] [node2] [path] [suffix]
+
+The script runs in one of two modes, depending on how many arguments you pass.
+
+Display Mode:
+For zero, one, or two arguments, faux_accio traverses the tree under [node1] and displays nodes with at least [n] total sublinks.  That is, the node and its children must have at least [n] nodes together in order to be shown.  This is an easy way to explore the ODP topictree.
+
+If [node1] is not given, the search starts at the tree's root.  If [n] is not given it defaults to 50,000.  [n] must be given in order to specify [node1].
+
+Ex:
+	faux_accio.py
+	faux_accio.py 10000
+	faux_accio.py 10000 Top/Regional/North_America/United_States/
+
+
+Download Mode:
+If [node2] is given, then faux_accio acquires all of the sublinks under [node1] and [node2] and randomly samples up to [n] from each.  Files are stored to the directory [path]/raw[suffix].  A download manifest is stored to directory [path]/fa_download[suffix].
+
+[path] defaults to the current directory.  If [path] or [path]/raw[suffix] does not exist, it is created. [suffix] defaults to ''.
+
+
+===== Notes =====
+* According to the authors' notes, the original Accio randomly selected nodes in the ODP tree for download.  I'm not that nice -- you have to choose your own nodes.  You can use the 
+
+* Downloading runs fast, with support for multiprocessing.
+
+* faux_accio.py uses the lxml library.  If it isn't available, it looks for the (much slower, but easier to install) elementtree library.
+
+
--- faux_accio.py
+++ faux_accio.py
@@ -1,6 +1,10 @@
 import random, re, datetime, pickle, time, csv, urllib2
-from lxml import etree
 import multiprocessing as mp
+try:
+    from lxml import etree
+except:
+    import elementtree.ElementTree as etree
+    
 ########################################################################
 
 pool_size = 100
@@ -53,171 +57,146 @@ def get_page_with_time_limit( url ):
 def process_page( (classname, classval, classid, url) ):
 #This is the workhorse function for this script.
 #It downloads a page and saves it, then returns a list with information
-        start_time = time.time()
-        filename = path+'c'+str(classval)+'d'+str(classid)+'.html'
+    start_time = time.time()
+    filename = path+'c'+str(classval)+'d'+str(classid)+'.html'
 
-        #Attempt to download the site
-        try:
-                (text, downloadSucceeded) = get_page_with_time_limit( url )
-        except:
-                downloadSucceeded = 0
-        
-        download_time = time.time()
-        length = len(text)
+    #Attempt to download the site
+    try:
+            (text, downloadSucceeded) = get_page_with_time_limit( url )
+    except:
+            downloadSucceeded = 0
+    
+    download_time = time.time()
+    length = len(text)
 
-        #Save the site
-        outfile = file(filename,'w')
-        outfile.write(text)
-        outfile.close()
+    #Save the site
+    outfile = file(filename,'w')
+    outfile.write(text)
+    outfile.close()
 
-        end_time = time.time()
+    end_time = time.time()
 
-        #Create the return array
-        download_result = [ classname, classval, classid, url, filename, start_time, download_time, end_time, downloadSucceeded, length ]
+    #Create the return array
+    download_result = [ classname, classval, classid, url, filename, start_time, download_time, end_time, downloadSucceeded, length ]
 #        print '\t', download_result
 
-        return download_result
+    return download_result
+
 
 
 def recursive_printer( node, min_count ):
-	if int(node.attrib['sublink_count']) >= min_count:
-		subtopics = 0
-		for c in node:
-			if c.tag=='Topic': subtopics+=1
-		print node.attrib['sublink_count'], '\t', subtopics, '\t', node.attrib['id']
-		
-		for c in node:
-			if c.tag=='Topic': recursive_printer( c, min_count )
-
+#Print out the number of own children, own links, and total links
+#for node and all its children with at least min_count total links
+    if int(node.attrib['sublink_count']) >= min_count:
+        subtopics = 0
+        for c in node:
+            if c.tag=='Topic': subtopics+=1
+        print subtopics, '\t', node.attrib['link_count'], '\t', node.attrib['sublink_count'], '\t', node.attrib['id']
+        
+        for c in node:
+            if c.tag=='Topic': recursive_printer( c, min_count )
 
 def find_topic( topic, root ):
-	root_id = root.attrib['id']
+#Locate a Topic node within the tree.  Returns None if the 
+    root_id = root.attrib['id']
 #	print topic, '\t', root_id
-	if not re.match( root_id, topic ): return None
+    if not re.match( root_id, topic ): return None
 
-	child_path = re.sub( root_id, '', topic )
-	if child_path == '': return root
+    child_path = re.sub( root_id, '', topic )
+    if child_path == '': return root
 
-	#Isolate the next step down the path
-	t = child_path.split('/')[1]
+    #Isolate the next step down the path
+    t = child_path.split('/')[1]
 
-	for n in root:
-		if n.tag == 'Topic':
+    for n in root:
+        if n.tag == 'Topic':
 #			print '\t', n.attrib['id'], '\t', root_id+'/'+t
-			if n.attrib['id'] == root_id+'/'+t:
-				return find_topic( topic, n )
+            if n.attrib['id'] == root_id+'/'+t:
+                return find_topic( topic, n )
 
-	return None
+    print 'WARNING: Topic node', topic, 'not found!'
+    return None
 
 def get_all_child_links( node ):
-	links = []
-	for n in node:
-		if n.tag == 'Topic': links = links + get_all_child_links( n )
-		if n.tag == 'link': links.append( n.attrib['resource'] )
-		if n.tag == 'link1': links.append( n.attrib['resource'] )
+#Get a list of all links under a Topic node and all of its children
+    links = []
+    for n in node:
+        if n.tag == 'Topic': links = links + get_all_child_links( n )
+        if n.tag == 'link': links.append( n.attrib['resource'] )
+        if n.tag == 'link1': links.append( n.attrib['resource'] )
 
-	return links
+    return links
 
 
 ########################################################################
 
 
-"""
-T = file( 'grep_topics.txt', 'r' ).read().split('\n')
+def main():
+    #load rdf file
+    print 'Loading rdf file...'
+    tree = etree.parse(file('content_tree.xml', 'r'))
+    root = tree.getroot()
 
-for t in T[:len(T)-1]:
-	t = t.split('"')[1]
-	depth = len(t.split('/'))
-	if depth < 4 and random.uniform(0,1) > .9:
-		print depth, t
-print len(T)
-"""
+    if 1:   #Display mode
+        pass
+    else:   #Download mode
+        print 'FInding nodes in tree...'
+        n1 = find_topic( class_1, root )
+        n2 = find_topic( class_2, root )
 
-"""
-#load rdf file
-print 'Loading file...'
-tree = etree.parse(file('content_tree.xml', 'r'))
-root = tree.getroot()
-"""
+        #Identify all pages under those nodes
+        print 'Identifying pages to crawl...'
+        l1 = get_all_child_links( n1 )
+        l2 = get_all_child_links( n2 )
 
-"""
-#pick nodes -- two siblings at depth d
-depth = 3
-parent = root
-for a in range(depth):
-	topic_children = []
-	for c in parent:
-		if c.tag == 'Topic': topic_children.append( c )
-	parent = random.choice( topic_children )
-	print parent
-	print parent.attrib['id']
-#	parent = random.choice( parent )
+        #sample pages
+        print 'Sampling pages...'
+        random.shuffle( l1 )
+        random.shuffle( l2 )
 
-topic_children = []
-for c in parent:
-	if c.tag == 'Topic': topic_children.append( c )
-child1 = random.choice( topic_children )
-while 1:
-	child2 = random.choice(topic_children)
-	if not child2 == child1: break
+        l1 = l1[:2000]
+        l2 = l2[:2000]
 
-print child1.attrib['id']
-print child2.attrib['id']
-"""
+        print l1[:50]
+        print len(l1)
+        print len(l2)
 
-"""
-print 'Picking nodes...'
-#n1 = 'Top/Games/Card_Games'
-#n2 = 'Top/Games/Dice'
+        pickle.dump( (l1, l2), file('lists.pkl', 'w') )
 
-n1 = find_topic( class_1, root )
-n2 = find_topic( class_2, root )
+        (l1, l2) = pickle.load( file('lists.pkl', 'r') )
 
-#identify all pages under those nodes
-print 'Identifying pages to crawl...'
-l1 = get_all_child_links( n1 )
-l2 = get_all_child_links( n2 )
+        #crawl
+        print 'Downloading pages...'
 
-#sample pages
-random.shuffle( l1 )
-random.shuffle( l2 )
+        start_time = datetime.datetime.now()
+        my_pool = mp.Pool(pool_size)		#Use pool size to gracefully handle latency
+        download_results = my_pool.map_async( process_page, [(class_1,1,a,l1[a]) for a in range(len(l1))] + [(class_2,2,a,l2[a]) for a in range(len(l2))], chunksize=1 )
+        n = len(l1)+len(l2)
+        while not download_results.ready():
+            download_results.wait(time_inc)
 
-l1 = l1[:2000]
-l2 = l2[:2000]
+            percent_complete = 1 - (float(download_results._number_left)/n)
+            if percent_complete > 0:
+                completion_duration = (datetime.datetime.now() - start_time)*10000/int(10000*percent_complete)
+                remaining_time = completion_duration*int(10000*(1-percent_complete))/10000
+                completion_time = (start_time+completion_duration).ctime()
+            else:
+                completion_duration = 'Estimate not available'
+                remaining_time = 'Estimate not available'
+                completion_time = 'Estimate not available'
+            print '\t', round( percent_complete*100, 1 ), '%\t\t', completion_duration, '\t\t', remaining_time, '\t\t', completion_time
+        download_results = download_results.get()
 
-print l1[:50]
-print len(l1)
-print len(l2)
+        print 'Saving results to csv...'
+        csv_file = file(path+'master_list.csv', 'wb')
+        csv_writer = csv.writer( csv_file )
+        csv_writer.writerow( [ 'classname', 'classval', 'classid', 'url', 'filename', 'start_time', 'download_time', 'end_time', 'downloadSucceeded', 'length' ] )
+        for d in download_results:
+            csv_writer.writerow( d )
+        csv_file.close()
 
-pickle.dump( (l1, l2), file('lists.pkl', 'w') )
-"""
-(l1, l2) = pickle.load( file('lists.pkl', 'r') )
 
-#crawl
-print 'Downloading pages...'
 
-start_time = datetime.datetime.now()
-my_pool = mp.Pool(pool_size)		#Use pool size to gracefully handle latency
-download_results = my_pool.map_async( process_page, [(class_1,1,a,l1[a]) for a in range(len(l1))] + [(class_2,2,a,l2[a]) for a in range(len(l2))], chunksize=1 )
-n = len(l1)+len(l2)
-while not download_results.ready():
-	download_results.wait(time_inc)
+if __name__ == "__main__":
+    main()
 
-	percent_complete = 1 - (float(download_results._number_left)/n)
-	if percent_complete > 0:
-		completion_duration = (datetime.datetime.now() - start_time)*10000/int(10000*percent_complete)
-		remaining_time = completion_duration*int(10000*(1-percent_complete))/10000
-		completion_time = (start_time+completion_duration).ctime()
-	else:
-		completion_duration = 'Estimate not available'
-		remaining_time = 'Estimate not available'
-		completion_time = 'Estimate not available'
-	print '\t', round( percent_complete*100, 1 ), '%\t\t', completion_duration, '\t\t', remaining_time, '\t\t', completion_time
-download_results = download_results.get()
-
-csv_file = file(path+'master_list.csv', 'wb')
-csv_writer = csv.writer( csv_file )
-csv_writer.writerow( [ 'classname', 'classval', 'classid', 'url', 'filename', 'start_time', 'download_time', 'end_time', 'downloadSucceeded', 'length' ] )
-for d in download_results:
-    csv_writer.writerow( d )
-csv_file.close()
